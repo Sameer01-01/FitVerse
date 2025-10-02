0b0af4b91958d1ee5d069bee08ecc723e48ed9ed
Aditya Kumar Singh <adityandmb@gmail.com>
2025-10-03 00:39:25 +0530

    Refactor code structure for improved readability and maintainability

--- HealthCare-Tracker/client/src/App.jsx
+++ HealthCare-Tracker/client/src/App.jsx
@@ -5,18 +5,19 @@ import Dietplans from "./components/Dietplans";
 import Injury from "./components/Injury";
 import Exercise from "./components/Exercise";
 import Nutrition from "./components/Nutrition";
-
+import Womenhealth from "./components/PeriodTracker";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Added default route */}
+        <Route path="/" element={<Home />} />  
         <Route path="/home" element={<Home />} />
         <Route path="/bmi" element={<Bmi />} />
         <Route path="/diet" element={<Dietplans />} />
         <Route path="/injury" element={<Injury />} />
         <Route path="/exercise" element={<Exercise />} />
         <Route path="/nutrition" element={<Nutrition />} />
+        <Route path="/period-tracker" element={<Womenhealth />} />
       </Routes>
     </BrowserRouter>
   );
